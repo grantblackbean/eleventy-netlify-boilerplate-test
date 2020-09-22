8779436862a1b47192ba98602904e03c816056d6
dana-blackbean <59980493+dana-blackbean@users.noreply.github.com>
2020-09-22 15:01:13 -0700

    Update index.js

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -1,11 +1,11 @@
 import Post from "/admin/preview-templates/post.js";
 import Page from "/admin/preview-templates/page.js";
-import Products from "/admin/preview-templates/product.js";
+import Product from "/admin/preview-templates/product.js";
 
 // Register the Post component as the preview for entries in the blog collection
 CMS.registerPreviewTemplate("blog", Post);
 CMS.registerPreviewTemplate("pages", Page);
-CMS.registerPreviewTemplate("product", Products);
+CMS.registerPreviewTemplate("product", Product);
 
 CMS.registerPreviewStyle("/_includes/assets/css/inline.css");
 // Register any CSS file on the home page as a preview style
