28b9a85578deb631b8f442bfaca9918ae7d79f6a
dana-blackbean <59980493+dana-blackbean@users.noreply.github.com>
2020-09-29 16:17:16 -0700

    Update page.js

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -3,18 +3,46 @@ import format from "https://unpkg.com/date-fns@2.7.0/esm/format/index.js?module"
 
 const html = htm.bind(h);
 
-// Preview component for a Page
-const Page = createClass({
+// Preview component for a Post
+const Post = createClass({
   render() {
     const entry = this.props.entry;
 
     return html`
       <main>
-        <h1>${entry.getIn(["data", "title"], null)}</h1>
-        <p>${entry.getIn(["data", "page_layout"], null)}</p>
+        <article>
+          <h1>${entry.getIn(["data", "title"], null)}</h1>
+          <p>
+            <small>
+              <time
+                >${
+                  format(
+                    entry.getIn(["data", "date"], new Date()),
+                    "dd MMM, yyyy"
+                  )
+                }</time
+              >
+              ${" by Author"}
+            </small>
+          </p>
+
+          <p>${entry.getIn(["data", "summary"], "")}</p>
+
+          ${this.props.widgetFor("body")}
+          <p>
+            ${
+              entry.getIn(["data", "tags"], []).map(
+                tag =>
+                  html`
+                    <a href="#" rel="tag">${tag}</a>
+                  `
+              )
+            }
+          </p>
+        </article>
       </main>
     `;
   }
 });
 
-export default Page;
+export default Post;
