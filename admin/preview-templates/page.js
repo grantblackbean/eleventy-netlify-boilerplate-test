0bb42ad6f18b0132a78164c722136c7f7282e416
dana-blackbean <59980493+dana-blackbean@users.noreply.github.com>
2020-09-29 15:27:06 -0700

    Update page.js

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -11,7 +11,6 @@ const Page = createClass({
     return html`
       <main>
         <h1>${entry.getIn(["data", "title"], null)}</h1>
-        ${this.props.widgetFor("body")}
         <p>${entry.getIn(["data", "page_layout"], null)}</p>
       </main>
     `;
