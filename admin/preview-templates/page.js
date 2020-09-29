49878dca2850045790ab76cb5a937f63006e0564
dana-blackbean <59980493+dana-blackbean@users.noreply.github.com>
2020-09-29 11:39:37 -0700

    test

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -10,7 +10,7 @@ const Page = createClass({
     return html`
       <main>
         <h1>${entry.getIn(["data", "title"], null)}</h1>
-        <p>${entry.getIn(["data", "page_layout"], null)}</p>
+        <p>${entry.getIn(["label", "page_layout"], null)}</p>
       </main>
     `;
   }
